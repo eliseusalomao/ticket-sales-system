045586579c13c3e1967763f3bab39c0cc0a5fd49
eliseusalomao <eliseusalomao20@gmail.com>
2024-06-22 14:55:48 -0300

    feat: add ticket validations and repository interface

--- golang-api/internal/events/domain/ticket.go
+++ golang-api/internal/events/domain/ticket.go
@@ -1,5 +1,11 @@
 package domain
 
+import "errors"
+
+var (
+	ErrPriceMustBeGreaterThanZero = errors.New("price must be greater than zero")
+)
+
 type TicketType string
 
 const (
@@ -13,4 +19,21 @@ type Ticket struct {
 	Spot       *Spot
 	TicketType TicketType
 	Price      float64
+}
+
+func IsValidTicketType(ticketType TicketType) bool {
+	return ticketType == TicketTypeHalf || ticketType == TicketTypeFull
+}
+
+func (t *Ticket) CalculatePrice() {
+	if t.TicketType == TicketTypeHalf {
+		t.Price /= 2
+	}
+}
+
+func (t *Ticket) Validate() error {
+	if t.Price <= 0 {
+		return ErrPriceMustBeGreaterThanZero
+	}
+	return nil
 }
